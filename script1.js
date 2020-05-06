6b007f19abdebdfe1ef6785c99649759e3ff974e
ZavadskiyAS <Zavadskiyweb@gmail.com>
2020-05-06 23:29:10 +0300

    task1

--- script1.js
+++ script1.js
@@ -1,2 +1,17 @@
-import users from './users.js';
-('use strict');
+'use strict';
+
+const categoriesList = document.querySelectorAll('.item');
+
+console.log(`В списке ${categoriesList.length} категории.`);
+
+const showCategories = allCategories => {
+  allCategories.forEach(item => {
+    const itemsTitle = item.querySelector('h2').textContent;
+    const itemsLength = item.querySelectorAll('li').length;
+    console.log(
+      `Категория: ${itemsTitle}
+Количество элементов: ${itemsLength}`,
+    );
+  });
+};
+showCategories(categoriesList);
