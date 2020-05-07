f892d8e7dd607e46b46a25c0702a289777026837
ZavadskiyAS <Zavadskiyweb@gmail.com>
2020-05-07 17:34:51 +0300

    addedTaska2

---
+++ script2.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const ingredients = [
+  'Картошка',
+  'Грибы',
+  'Чеснок',
+  'Помидоры',
+  'Зелень',
+  'Приправы',
+];
+const ingredientsList = document.querySelector('#ingredients');
+const createIngredientsItems = ingredientsArr => {
+  return ingredientsArr.reduce((listItem, ingredient) => {
+    const item = document.createElement('li');
+    item.textContent = ingredient;
+    listItem.push(item);
+    return listItem;
+  }, []);
+};
+const newList = createIngredientsItems(ingredients);
+ingredientsList.append(...newList);
